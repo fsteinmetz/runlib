9c184511c418580328c11d41717e49a4c4d12738
François Steinmetz <fs@hygeos.com>
2015-02-09 15:51:03 +0100

    condorstatus.py: show (average) job priority

--- condorstatus.py
+++ condorstatus.py
@@ -18,23 +18,27 @@ if __name__ == '__main__':
     for line in out.split('\n'):
 
         try:
-            id, owner, _, _, _, st = line.split()[:6]
+            id, owner, _, _, _, st, prio = line.split()[:7]
         except ValueError:
             continue
         if not '.' in id: continue
 
         cl = id.split('.')[0]
 
-        # clusters = [owner, total, running]
+        # clusters = [owner, total, running, sum_prio]
         try:
             clusters[cl][1] += 1
         except KeyError:
-            clusters[cl] = [owner, 1, 0]
+            clusters[cl] = [owner, 1, 0, 0.]
         if st == 'R':
             clusters[cl][2] += 1
+        clusters[cl][3] += float(prio)
 
-    w1, w2 = 10, 12
-    print 'CLUSTER'.ljust(w1), 'OWNER'.ljust(w2), 'RUNNING'
+    w1, w2, w3 = 10, 12, 10
+    print 'CLUSTER'.ljust(w1), 'OWNER'.ljust(w2), 'RUNNING'.ljust(w3), 'PRIORITY'
     for cl in clusters.keys():
-        print cl.ljust(w1), clusters[cl][0].ljust(w2), '%d/%d' % (clusters[cl][2], clusters[cl][1])
+        print cl.ljust(w1), \
+                clusters[cl][0].ljust(w2), \
+                ('%d/%d' % (clusters[cl][2], clusters[cl][1])).ljust(w3), \
+                clusters[cl][3]/clusters[cl][1]
 
