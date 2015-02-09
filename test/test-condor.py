025c0b86efe3eb9858acec189293af4efb49fb87
François Steinmetz <fs@hygeos.com>
2015-02-09 15:43:23 +0100

    Add test for passing custom objects

--- test/test-condor.py
+++ test/test-condor.py
@@ -6,12 +6,17 @@
 import sys
 from os.path import abspath, dirname
 sys.path.append(dirname(dirname(abspath(__file__)))) # add parent directory to the path
+from time import sleep
 
 from condor import CondorPool as Pool
 # from multiprocessing import Pool
 
-from time import sleep
-from numpy import eye
+
+class Foo(object):
+    def __init__(self, a):
+        self.__a = a
+    def test(self):
+        return self.__a
 
 def f(x):
     sleep(x)
@@ -20,6 +25,9 @@ def f(x):
 def g(x, y):
     sleep(x)
     return x**2 + y**2
+
+def fobj(obj):
+    return obj.test()
 
 def test1():
     print('\ntest 1: map')
@@ -38,14 +46,22 @@ def test3():
     print('->', sum(pool.map(g, list(range(10)), list(range(20, 30)))))
 
 def test4():
-    print('\ntest 3: imap_unordered with 2 arguments')
+    print('\ntest 4: imap_unordered with 2 arguments')
     pool = Pool()
     it = pool.imap_unordered(g, list(range(10)), list(range(20, 30)))
     print('->',  sum(it))
 
+def test5():
+    print('test 5: custom objects')
+    objects = map(Foo, range(10))
+
+    pool = Pool(add_globals=['Foo'])
+    print pool.map(fobj, objects)
+
 if __name__ == '__main__':
 
-    test1()
-    test2()
-    test3()
-    test4()
+    # test1()
+    # test2()
+    # test3()
+    # test4()
+    test5()
