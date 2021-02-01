64af84c29eb8b5319a7619a07234081dbe85161c
François Steinmetz <fs@hygeos.com>
2021-02-01 10:18:05 +0100

    Update test_condor

--- tests/test_condor.py
+++ tests/test_condor.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+'''
+Run several condor tests
+
+Note: does not work through pytest. Should be run as a standard script.
+'''
+
 from runlib.condor import CondorPool
 from time import sleep
+import getpass
 
 def f(x):
-    sleep(x)
     return x**2
 
-def test_condor_map():
-    print('\ntest 1: map')
+def getuser(x):
+    return getpass.getuser()
+
+
+def _test_condor_map():
     pool = CondorPool()
-    print('->', sum(pool.map(f, list(range(10)))))
+    assert sum(pool.map(f, range(10))) == sum(map(f, range(10)))
+
+def _test_condor_user():
+    """
+    Check that job runs as the same user as  main script
+    """
+    res = CondorPool().map(getuser, range(10))
+    assert res[0] == getuser(0), f'{res[0]} != {getuser(0)}'
 
 
 
 if __name__ == '__main__':
-    test_condor_map()
+    _test_condor_map()
+    _test_condor_user()
